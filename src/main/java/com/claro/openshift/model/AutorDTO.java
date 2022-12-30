3cf1bd991c9e90cb2343bf4361dfa5bf1ecd4052
ECM5242N <ECM5242N@claro.com.co>
2022-12-30 12:04:59 -0500

    sdfsefsfghtgfh

--- src/main/java/com/claro/openshift/model/AutorDTO.java
+++ src/main/java/com/claro/openshift/model/AutorDTO.java
@@ -2,33 +2,37 @@ package com.claro.openshift.model;
 
 public class AutorDTO {
     
-    private int id;
-    private String nombre; 
+    private int id_autor;
+    private String nombre_autor; 
 
-    public AutorDTO() {
+
+
+    public AutorDTO(int id_autor, String nombre_autor) {
+        this.id_autor = id_autor;
+        this.nombre_autor = nombre_autor;
     }
 
-    public AutorDTO(int id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
+
+
+    public int getId_autor() {
+        return id_autor;
     }
-    public int getId() {
-        return id;
+
+    public void setId_autor(int id_autor) {
+        this.id_autor = id_autor;
     }
-    public void setId(int id) {
-        this.id = id;
+
+    public String getNombre_autor() {
+        return nombre_autor;
     }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+
+
+
+    public void setNombre_autor(String nombre_autor) {
+        this.nombre_autor = nombre_autor;
     }
 
 
     
-     
-    
-
 
 }
